4aeac2d8d73f64f90c7674a1e8e9fc0abe7fb9c4
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-07-08 12:13:49 +0200

    updated replay script

--- scripts/replay.rb
+++ scripts/replay.rb
@@ -2,32 +2,40 @@
 
 require 'orocos'
 require 'orocos/log'
-#require 'vizkit'
+require 'rock/bundle'
+require 'vizkit'
 
 include Orocos
 
 BASE_DIR = File.expand_path('..', File.dirname(__FILE__))
 ENV['PKG_CONFIG_PATH'] = "#{BASE_DIR}/build:#{ENV['PKG_CONFIG_PATH']}"
 
-Orocos.initialize
-Orocos.conf.load_dir("#{ENV['AUTOPROJ_PROJECT_BASE']}/bundles/asguard/config/orogen")
+Bundles.initialize
 
-Orocos.run 'test_trajectory' do |p|
-    Orocos.log_all_ports 
-    tf = p.task 'trajectory'
-    Orocos.conf.apply(tf, ['default'])
+Orocos.run 'trajectory_follower::Task' => 'tf' do |p|
+#    Orocos.log_all_ports 
+    tf = p.task 'tf'
+    tf.apply_conf(['default'])
 
     if ARGV.size == 0 then
-	log_replay = Orocos::Log::Replay.open( "corridor_following.0.log", "eslam_pose_estimator.0.log" ) 
+	log_replay = Orocos::Log::Replay.open( "item_follower.0.log", "item_planner.0.log" ) 
     else
-	log_replay = Orocos::Log::Replay.open( ARGV[0]+"corridor_following.0.log" , ARGV[0]+"eslam_pose_estimator.0.log" ) 
+	log_replay = Orocos::Log::Replay.open( ARGV[0]+"/item_velodyne.0.log" , ARGV[0]+"/item_planner.0.log" ) 
     end
-    log_replay.corridor_following.trajectory.connect_to( tf.trajectory, :type => :buffer, :size => 1 )
-    log_replay.eslam_pose_estimator.pose_samples.connect_to( tf.pose, :type => :buffer, :size => 1 )
+    log_replay.global_planner.trajectory.connect_to( tf.trajectory, :type => :buffer, :size => 1 )
+    log_replay.velodyne_slam.pose_samples.connect_to( tf.pose, :type => :buffer, :size => 1 )
     
     tf.configure
     tf.start
 
-    log_replay.run
+    Vizkit.display log_replay.velodyne_slam.pose_samples, :widget => Vizkit.default_loader.RigidBodyStateVisualization
+    Vizkit.display log_replay.global_planner.trajectory
+    
+    Vizkit.display tf.currentCurvePointWp
+
+    
+    Vizkit.control log_replay
+    Vizkit.exec
+
 end
 
